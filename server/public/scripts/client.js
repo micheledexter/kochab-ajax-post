c033241b311e7375cdedd1046d371af655c97203
Michele Dexter <micheleannedexter@gmail.com>
2018-04-19 12:00:59 -0500

    display record information in table and list

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -12,11 +12,27 @@ $.ajax({
 }).then(function (response) {
     $('body').append('<div id="records"></div>');
     $('#records').append('<h2>Record Collection</h2>')
+
+    // Unordered List
+    $('#records').append('<h3>List</h3>');
+    $('#records').append('<ul></ul>');
     for (let record of response) {
         let title = record.title;
         let year = record.year;
         let artist = record.artist;
-        let cost = `$${record.cost.toFixed(2).toLocaleString()}`;
-        $('#records').append(`<p>"${title}" (${year}) - by ${artist}: ${cost}</p>`);
+        let cost = record.cost.toLocaleString('en', {style: 'currency', currency: 'USD'});
+        $('ul').append(`<li>"${title}" (${year}) - by ${artist}: ${cost}</li>`);
     }
+    
+    // Table
+    $('#records').append('<h3>Table</h3>');
+    $('#records').append('<table><thead><tr><th>Title</th><th>Artist</th><th>Year</th><th>Cost</th></tr></thead><tbody></tbody></table>');
+
+    response.forEach(function(record) {
+        let title = record.title;
+        let year = record.year;
+        let artist = record.artist;
+        let cost = `$${record.cost.toFixed(2).toLocaleString()}`;
+        $('tbody').append(`<tr><td>${title}</td><td>${artist}</td><td>${year}</td><td>${cost}</td></tr>`);
+    });
 });
